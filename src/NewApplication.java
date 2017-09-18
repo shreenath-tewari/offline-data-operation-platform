715e1c23bb6e650c86b34eacaa099f3297dc0f88
Shreenath Tewari <stewari.sps@gmail.com>
2017-09-18 10:42:45 +0530

    basic layout set

--- src/NewApplication.java
+++ src/NewApplication.java
@@ -1,5 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
 
+public class NewApplication extends JFrame {
 
-public class NewApplication {
-    void NewApplication
+    JLabel applicationTitle;
+    JPanel containerPane, fieldElements;
+    JButton saveButton;
+
+    NewApplication() {
+
+        applicationTitle = new JLabel("Offline Data Operations Platform");
+        applicationTitle.setHorizontalAlignment(SwingConstants.CENTER);
+
+        containerPane = new JPanel(new BorderLayout());
+        fieldElements =new JPanel();
+
+        saveButton = new JButton("Save");
+
+        addElementsToContainerPane();
+
+        showApplication();
+    }
+
+    void addElementsToContainerPane() {
+
+        containerPane.add(applicationTitle, BorderLayout.PAGE_START);
+        containerPane.add(fieldElements, BorderLayout.CENTER);
+        containerPane.add(saveButton, BorderLayout.PAGE_END);
+
+    }
+
+    void showApplication() {
+
+        add(containerPane);
+        setSize(500, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        new NewApplication();
+
+    }
+
 }
