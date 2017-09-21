9aebf9bab07189ad08ff5df0aa40b04d1fa75609
Shreenath Tewari <stewari.sps@gmail.com>
2017-09-21 23:25:31 +0530

    basic part of new application done

--- src/NewApplication.java
+++ src/NewApplication.java
@@ -1,32 +1,137 @@
+import java.io.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
 
-public class NewApplication extends JFrame {
+public class NewApplication extends JFrame{
 
-    JLabel applicationTitle;
-    JPanel containerPane, fieldElements;
-    JButton saveButton;
+    int fieldCounter;
+
+    JLabel applicationTitle, applicationNameLabel;
+
+    JTextField applicationNameTextField;
+
+    JPanel containerPane, fieldElements, footerPanel;
+
+    JButton saveButton, addFieldButton;
+
+    JScrollPane fieldElementsScrollPane;
+
+    ArrayList<String> fieldName;
+    ArrayList<String> fieldType;
+    ArrayList<JLabel> fieldNameLabel;
+    ArrayList<JLabel> fieldTypeLabel;
+    ArrayList<JTextField> fieldNameTextField;
+    ArrayList<JRadioButton> textField;
+    ArrayList<JRadioButton> numberField;
+    ArrayList<JRadioButton> listField;
+    ArrayList<String> optionChosen;
 
     NewApplication() {
 
-        applicationTitle = new JLabel("Offline Data Operations Platform");
-        applicationTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        fieldCounter = -1;
 
         containerPane = new JPanel(new BorderLayout());
-        fieldElements =new JPanel();
 
+        footerPanel = new JPanel(new GridLayout(1, 2));
+
+        applicationTitle = new JLabel("Offline Data Operations Platform - New Application");
+        applicationTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        fieldElements =new JPanel();
+        fieldElements.setLayout(new GridLayout(40,1));
         saveButton = new JButton("Save");
+
+        fieldElementsScrollPane = new JScrollPane(fieldElements);
+
+        applicationNameLabel = new JLabel("Enter Application Name");
+        applicationNameTextField = new JTextField();
+
+        fieldElements.add(applicationNameLabel);
+        fieldElements.add(applicationNameTextField);
+
+        fieldName = new ArrayList<>();
+        fieldType = new ArrayList<>();
+
+        fieldNameLabel = new ArrayList<>();
+        fieldTypeLabel = new ArrayList<>();
+        fieldNameTextField = new ArrayList<>();
+        textField = new ArrayList<>();
+        numberField = new ArrayList<>();
+        listField = new ArrayList<>();
+        addFieldButton = new JButton("Add Another Field");
+
+        footerPanel.add(saveButton);
+        footerPanel.add(addFieldButton);
+
+        addElementsToFieldElementsPane();
 
         addElementsToContainerPane();
 
         showApplication();
+
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                saveApplication();
+            }
+        });
+    }
+
+    void addElementsToFieldElementsPane() {
+
+        fieldCounter++;
+        addNewFields(fieldCounter);
+
+        addFieldButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addNewFields(fieldCounter++);
+
+            }
+        });
+    }
+
+    void addNewFields(int c) {
+        fieldNameLabel.add(new JLabel("Enter Field Name"));
+        fieldNameTextField.add(new JTextField());
+        fieldTypeLabel.add(new JLabel("Enter Field Type"));
+        textField.add(new JRadioButton("Text"));
+        numberField.add(new JRadioButton("Number"));
+        listField.add(new JRadioButton("List"));
+        fieldElements.add(fieldNameLabel.get(c));
+        fieldElements.add(fieldNameTextField.get(c));
+        fieldElements.add(fieldTypeLabel.get(c));
+        fieldElements.add(textField.get(c));
+        fieldElements.add(numberField.get(c));
+        //fieldElements.add(listField.get(c));
+
+        setOptionChooser(c);
+
+    }
+
+    void setOptionChooser(int c) {
+        textField.get(c).addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                fieldType.add("Text");
+            }
+        });
+        numberField.get(c).addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                fieldType.add("Number");
+            }
+        });
     }
 
     void addElementsToContainerPane() {
 
         containerPane.add(applicationTitle, BorderLayout.PAGE_START);
-        containerPane.add(fieldElements, BorderLayout.CENTER);
-        containerPane.add(saveButton, BorderLayout.PAGE_END);
+        containerPane.add(fieldElementsScrollPane, BorderLayout.CENTER);
+        containerPane.add(footerPanel, BorderLayout.PAGE_END);
 
     }
 
@@ -37,6 +142,29 @@ public class NewApplication extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
 
+    }
+
+    void saveApplication() {
+
+        int i;
+        String line, fileName;
+        for(i = 0; i < fieldNameLabel.size(); i++) {
+            fieldName.add(fieldNameTextField.get(i).getText());
+        }
+
+        fileName = applicationNameTextField.getText() + ".txt";
+        FileOutputStream fout;
+
+        try {
+            fout = new FileOutputStream(fileName);
+            for(i = 0; i < fieldNameLabel.size(); i++) {
+                line = fieldName.get(i) + "/" + fieldType.get(i) + "\n";
+                byte b[] = line.getBytes();
+                fout.write(b);
+            }
+            fout.close();
+        }
+        catch (Exception e){};
     }
 
     public static void main(String[] args) {
